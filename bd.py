39618971002e28f97d82d9bb3313132f87fc4a19
Daniel_CM <daniel_200589@hotmail.com>
2021-10-24 01:15:45 -0500

    add todo and check todos in db

--- bd.py
+++ bd.py
@@ -21,4 +21,47 @@ crs.execute("""
 
 connec.commit()
 
+def render_todos():
+    rows = crs.execute("SELECT * FROM todo").fetchall()
+    print(len(rows))
+    print(rows)
+    
+    for i in range(0, len(rows)):
+        completed = rows[i][3]
+        description = rows[i][2]
+        cb = Checkbutton(frame, text=description, width=42, anchor="w")
+        cb.grid(row=i, column=0, sticky="w")
+
+
+def add_todo():
+    todo = todo_entry.get()
+    if todo:
+        crs.execute("""
+                INSERT INTO todo (description, completed) VALUES (?, ?)
+        """, (todo, False))
+        connec.commit()
+        todo_entry.delete(0, "end")
+        render_todos()
+    else:
+        pass
+
+
+label_name = Label(window, text="Tarea")
+label_name.grid(row=0, column=0)
+
+todo_entry = Entry(window, width=40)
+todo_entry.grid(row=0, column=1)
+
+btn = Button(window, text="Agregar", command=add_todo)
+btn.grid(row=0, column=2)
+
+frame = LabelFrame(window, text="Mis tareas", padx=5, pady=5)
+frame.grid(row=1, column=0, columnspan=3, sticky="nswe", padx=5)
+
+todo_entry.focus()
+
+
+window.bind('<Return>', lambda x: add_todo())
+
+render_todos()
 window.mainloop()
